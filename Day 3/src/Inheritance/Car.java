77c9b87491e80c76e32e61a7f0a24a1172a5681b
Deniza <elada.baltika@gmail.com>
2022-08-18 12:26:06 +0300

    last update

--- Day 3/src/Inheritance/Car.java
+++ Day 3/src/Inheritance/Car.java
@@ -11,10 +11,3 @@ public class Car extends Vehicle {
         this.gear = gear;
     }
 }
-//5) Inheritance.Vehicle
-// Create a base class of a Inheritance.Vehicle,
-// then create a Inheritance.Car class which inherits from the Inheritance.Vehicle class.
-// Also, create another class, a specific type of Inheritance.Car that inherits from the Inheritance.Car class.
-// Program should be able to handle steering, changing gears, and drive (having speed in other words).
-// You might decide where to put the appropriate state and behaviors (fields and methods).
-// As mentioned above, changing gears, increasing/decreasing speed of driving should be included.
