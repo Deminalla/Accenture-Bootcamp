a579387db1bdd7822af062698ec479e88a030943
Deniza <elada.baltika@gmail.com>
2022-08-19 12:03:40 +0300

    treeset update

--- Day 4/src/treeSet.java
+++ Day 4/src/treeSet.java
@@ -2,7 +2,32 @@ import java.util.*;
 
 public class treeSet {
     public static void main(String[] args) {
-        TreeSet<String> countryList = new TreeSet<String>();
+        TreeSet<String> countryList = new TreeSet<>(); // self-balancing binary search tree
+        countryList.add("Lithuania");
+        countryList.add("Latvia");
+        countryList.add("Germany");
+        countryList.add("Italy");
+        countryList.add("Japan");
 
+        for(String element: countryList){
+            System.out.println(element); // Prints out from the smallest element (starts from the very left of the tree)
+        }
+
+        // Find countries that are less than K (it's more comfortable to do this with numbers but oh well)
+        TreeSet<String> searchList = new TreeSet<>();
+        searchList = (TreeSet)countryList.headSet("K");
+
+        Iterator it = searchList.iterator();
+        /*while (it.hasNext()){
+            System.out.println(it.next() + " ");
+        }*/
+
+        System.out.println("Greater or equal to K: " + countryList.ceiling("K")); // will show only 1 element (because this is a binary tree)
+        System.out.println("Greater than K: " + countryList.higher("K"));
+        System.out.println("ess or equal to K: " + countryList.floor("K"));
+        System.out.println("Less than K: " + countryList.lower("K"));
+
+        System.out.println("Removes the first(lowest) element: "+countryList.pollFirst());
+        System.out.println("Removes Latvia from the list: "+countryList.remove("Latvia"));
     }
 }
