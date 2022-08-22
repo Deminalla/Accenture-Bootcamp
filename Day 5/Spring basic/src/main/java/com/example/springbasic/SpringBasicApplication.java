24fc40a87a03aa66228b453b7599386368f6f5aa
Deniza <elada.baltika@gmail.com>
2022-08-22 13:50:49 +0300

    commit

---
+++ Day 5/Spring basic/src/main/java/com/example/springbasic/SpringBasicApplication.java
@@ -0,0 +1,25 @@
+package com.example.springbasic;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBasicApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicApplication.class, args);
+    }
+
+    @GetMapping("/hello") // it will expect the path http://localhost:8080/hello?name=Den
+    // only supports get type http and is shortcut for @RequestMapping(method = requestMethod.GET, path = "/hello")
+    //because RequestMapping also allows cutomizing and bby default allows different http types like post, delete and so on
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) { // in case we don't input/gave the name it will be World
+        return String.format("Hello %s!", name);
+    }
+
+}
+            
