e1386496b0b069510022e06529685ab7ca4a4ed8
Deniza <elada.baltika@gmail.com>
2022-08-22 11:10:23 +0300

    junit testing from day 5

--- Day 2/src/Main.java
+++ Day 2/src/Main.java
@@ -5,12 +5,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);  // input stream
 
+
         //compareIntegers(input);
-        //compareBoolean(input);
-        //evenOrOdd1(input);
-        //evenOrOdd2(input);
-        //booleanOpposite(input);
-        //integerTrue(input);
+
+        System.out.print("Enter b1: ");
+        boolean b1 = Boolean.parseBoolean(input.nextLine());
+        System.out.print("Enter b2: ");
+        boolean b2 = Boolean.parseBoolean(input.nextLine());
+        //compareBoolean(b1, b2);
+
+        System.out.print("Enter a whole number: ");
+        int number1 = Integer.parseInt(input.nextLine());
+        //evenOrOdd1(number1);
+
+        System.out.print("Enter a whole number: ");
+        int number2 = Integer.parseInt(input.nextLine());
+        //evenOrOdd2(number2);
+
+        System.out.print("Enter boolean value: ");
+        boolean value = Boolean.parseBoolean(input.nextLine());
+        //booleanOpposite(value);
+
+        System.out.print("Enter value a: ");
+        int a = Integer.parseInt(input.nextLine());
+
+        System.out.print("Enter value b: ");
+        int b = Integer.parseInt(input.nextLine());
+        //integerTrue(a, b);
 
         input.close();
     }
@@ -18,7 +39,6 @@ public class Main {
     static void compareIntegers (Scanner input) {
         System.out.print("Enter value a: ");
         int a = Integer.parseInt(input.nextLine());
-
         System.out.print("Enter value b: ");
         int b = Integer.parseInt(input.nextLine());
 
@@ -29,49 +49,34 @@ public class Main {
         System.out.println("is " + a + " greater or equal to " + b + " ? - " + (a>=b));
     }
 
-    static void compareBoolean (Scanner input) {
-        System.out.print("Enter b1: ");
-        boolean b1 = Boolean.parseBoolean(input.nextLine());
-
-        System.out.print("Enter b2: ");
-        boolean b2 = Boolean.parseBoolean(input.nextLine());
-
+    static boolean compareBoolean (boolean b1, boolean b2) {
         System.out.println("Is " + b1 + " and " + b2 + " equal? - " + (b1==b2));
+        return (b1==b2);
     }
 
-    static void evenOrOdd1 (Scanner input) {
-        System.out.print("Enter a whole number: ");
-        int number = Integer.parseInt(input.nextLine());
-
-        System.out.println("Is " + number + " even? - " + (number%2==0));
+    static boolean evenOrOdd1 (int number1) {
+        System.out.println("Is " + number1 + " even? - " + (number1%2==0));
+        return (number1%2==0);
     }
 
-    static void evenOrOdd2 (Scanner input) {
-        System.out.print("Enter a whole number: ");
-        int number = Integer.parseInt(input.nextLine());
-
-        System.out.println("Is " + number + " even? - " + (number%2!=0));
+    static boolean evenOrOdd2 (int number2) {
+        System.out.println("Is " + number2 + " even? - " + (number2%2!=0));
+        return (number2%2!=0);
     }
 
-    static void booleanOpposite (Scanner input) {
-        System.out.print("Enter boolean value: ");
-        boolean value = Boolean.parseBoolean(input.nextLine());
-
+    static boolean booleanOpposite (boolean value) {
         System.out.println("The opposite of " + value + " is " + !value);
+        return !value;
     }
 
-    static void integerTrue (Scanner input) {
-        System.out.print("Enter value a: ");
-        int a = Integer.parseInt(input.nextLine());
-
-        System.out.print("Enter value b: ");
-        int b = Integer.parseInt(input.nextLine());
-
+    static boolean integerTrue (int a, int b) {
         if((a==b) || (a<0 && b>0) || (a>100 && b>100)) {
             System.out.println("true");
+            return true;
         }
         else{
             System.out.println("false");
+            return false;
         }
     }
 }
---
+++ Day 2/src/MainTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    @DisplayName("Compare two boolean statements")
+    void compareBoolean() {
+        assertFalse(Main.compareBoolean(true, false));
+        assertFalse(Main.compareBoolean(false, true));
+        assertTrue(Main.compareBoolean(true, true));
+    }
+
+    @Test
+    @DisplayName("Is the number even?")
+    void evenOrOdd1() {
+        assertAll(()-> assertEquals(true, Main.evenOrOdd1(10)), // can shorten it to assertFalse and assertTrue
+                ()-> assertEquals(true, Main.evenOrOdd1(78)),
+                ()-> assertEquals(false, Main.evenOrOdd1(15)),
+                ()-> assertEquals(false, Main.evenOrOdd1(13)));
+    }
+
+    @Test
+    @DisplayName("Is the number odd?")
+    void evenOrOdd2() {
+        assertFalse(Main.evenOrOdd2(10));
+        assertTrue(Main.evenOrOdd2(7));
+    }
+
+    @Test
+    @DisplayName("The opposite of given boolean")
+    void booleanOpposite() {
+        assertFalse(Main.booleanOpposite(true));
+        assertTrue(Main.booleanOpposite(false));
+    }
+
+    @Test
+    @DisplayName("(a==b) || (a<0 && b>0) || (a>100 && b>100)")
+    void integerTrue() {
+        assertAll(()-> assertEquals(false, Main.integerTrue(-1, -2)),
+                ()-> assertEquals(true, Main.integerTrue(-2, 1)),
+                ()-> assertEquals(true, Main.integerTrue(-1, -1)),
+                ()-> assertEquals(true, Main.integerTrue(101, 102)),
+                ()-> assertEquals(false, Main.integerTrue(99, 200)));
+    }
+}
