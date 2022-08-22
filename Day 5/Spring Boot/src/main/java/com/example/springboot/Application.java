6433b15e9fd55dfed806159d530d4336907be03d
Deniza <elada.baltika@gmail.com>
2022-08-22 14:54:36 +0300

    spring explanation

--- Day 5/Spring Boot/src/main/java/com/example/springboot/Application.java
+++ Day 5/Spring Boot/src/main/java/com/example/springboot/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// includes @Configuration (Tags the class as a source of bean definitions for the application context),
+// @EnableAutoConfiguration,
+// @ComponentScan (Tells Spring to look for other components, configurations, and services in the com/example package, letting it find the controllers)
 public class Application {
 
     public static void main(String[] args) {
--- Day 5/Spring Boot/src/main/java/com/example/springboot/GreetingController.java
+++ Day 5/Spring Boot/src/main/java/com/example/springboot/GreetingController.java
@@ -10,10 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private final AtomicLong counter = new AtomicLong(); // it's not a replacement for long int (good with multiple threads?)
+    // it's an atomic variable
 
     @GetMapping("/greeting") // ensures that HTTP GET requests to "/greeting" are mapped to the greeting() method
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        // returns a new Greeting object with id and content attributes based on the next value from the counter and formats the given name according to template
+        // If you open the website 2 times, the id will have changed from 1 to 2.
+        // This proves you're working against the same GreetingController across multiple requests and that its counter is incremented on each call
     }
 }
