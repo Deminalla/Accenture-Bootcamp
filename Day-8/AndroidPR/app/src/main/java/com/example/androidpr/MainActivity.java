7519e31932e548d88086dac2897665dff96e0845
Deniza <elada.baltika@gmail.com>
2022-08-25 15:16:27 +0300

    button moves to new section

--- Day-8/AndroidPR/app/src/main/java/com/example/androidpr/MainActivity.java
+++ Day-8/AndroidPR/app/src/main/java/com/example/androidpr/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.androidpr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.androidpr.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) { // when the user pressses the button
+        Intent intent = new Intent(this, DisplayMessageActivity.class); // The Intent constructor takes 2 parameters, a Context and a Class.
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
